468bbb998390427caae9271a40085d99b8394ae6
Rosta Striz <rostislavs@kentico.com>
2022-02-15 15:05:16 +0100

    post to api

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,9 @@
 module.exports = async function (context, req) {
+  const body = req.body;
+  const headers = req.headers;
+  
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: { text: "Hello from the API. Secret is: " + process.env.secret },
+    body: { text: "Hello from the API. Secret is: " + process.env.secret + " headers token: " + req.headers.token + " body: " + JSON.stringify(req.body)},
   };
 };
